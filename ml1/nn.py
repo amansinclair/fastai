429be3a20c00a36efef74530205deb25e825b9fe
Alex Sinclair <amansinclair@gmail.com>
2020-03-13 20:08:04 +0100

    working on backprop

--- ml1/nn.py
+++ ml1/nn.py
@@ -2,17 +2,42 @@ import numpy as np
 
 
 class Layer:
-    def __init__(self, input_size, output_size, bias=True):
+    def __init__(self, input_size, output_size):
         self.W = np.random.rand(output_size, input_size)
         self.b = np.zeros(output_size)
         self.grad = None
 
     def __call__(self, X):
-        self.grad = X
+        self.input = X.copy()
         return np.dot(X, self.W.T) + self.b
 
     def update(self, loss, learning_rate):
-        pass
+        """ loss: batch_size x output_size"""
+        layer_loss = loss * self.W
+        self.W += learning_rate * (self.input * loss)
+        self.b += learning_rate * (loss)
+        return layer_loss
+
+
+class ReLU:
+    def __call__(self, X):
+        self.input = X.copy()
+        X[X < 0] = 0
+        return X
+
+    def update(self, loss):
+        self.input[self.input > 0] = 1
+        self.input[self.input < 0] = 0
+        return self.input * loss
+
+
+class MSE:
+    def __call__(self, y_true, y_pred):
+        self.loss = -2 * (y_true - y_pred)
+        return np.mean((y_true - y_pred) ** 2)
+
+    def update(self):
+        return self.loss
 
 
 if __name__ == "__main__":
