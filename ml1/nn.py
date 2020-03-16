f74f96743deb914a223e7708ace1753872f49b4b
Alex Sinclair <amansinclair@gmail.com>
2020-03-16 20:48:34 +0100

    nn tested and working

--- ml1/nn.py
+++ ml1/nn.py
@@ -3,7 +3,24 @@ import torch.nn as nn
 import torch
 
 
-class Layer:
+class Sequential:
+    def __init__(self, *layers):
+        self.layers = layers
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def __getitem__(self, index):
+        return self.layers[index]
+
+    def backward(self, dloss):
+        for layer in reversed(self.layers):
+            dloss = layer.backward(dloss)
+
+
+class Linear:
     def __init__(self, input_size, output_size):
         self.weight = np.random.rand(output_size, input_size)
         self.bias = np.zeros(output_size)
@@ -16,13 +33,19 @@ class Layer:
         return np.dot(X, self.weight.T) + self.bias
 
     def backward(self, loss):
-        "loss.shape - > batch_size x output_size"
+        """loss.shape -> batch_size x output_size"""
         self.bias_grad = np.mean(loss, axis=0)
         self.weight_grad = np.dot(loss.T, self.inputs) / loss.shape[0]
+        loss_return = np.dot(loss, self.weight)
+        return loss_return
 
-    def update(self, learning_rate):
-        self.W += learning_rate * (self.weight_grad)
-        self.b += learning_rate * (self.bias_grad)
+    def update(self, learning_rate=0.00001):
+        self.weight += learning_rate * (self.weight_grad)
+        self.bias += learning_rate * (self.bias_grad)
+
+    def get_weights(self, layer):
+        self.weight = layer.weight.data.numpy()
+        self.bias = layer.bias.data.numpy()
 
 
 class ReLU:
@@ -47,27 +70,30 @@ class MSE:
 
 
 if __name__ == "__main__":
-    net = nn.Sequential(nn.Linear(3, 1))
+    net = nn.Sequential(nn.Linear(3, 2), nn.ReLU(), nn.Linear(2, 1))
     net.zero_grad()
-    my_net = Layer(3, 1)
-    my_net.weight = net[0].weight.data.numpy()
-    my_net.bias = net[0].bias.data.numpy()
+    l1 = Linear(3, 2)
+    l1.get_weights(net[0])
+    l2 = Linear(2, 1)
+    l2.get_weights(net[2])
     X = np.array([[0, 1, 2], [0, 1, 2]], dtype="float32")
     y = np.array([[0.1], [1.2]], dtype="float32")
     Xt = torch.Tensor(X)
     yt = torch.Tensor(y)
     crit = nn.MSELoss()
     my_crit = MSE()
+    rl = ReLU()
+    my_net = Sequential(l1, rl, l2)
     o = net(Xt)
     my_o = my_net(X)
     print(o, my_o)
     loss = crit(yt, o)
     my_loss = my_crit(y, my_o)
-    print(loss, my_loss)
+    print("LOSSES", loss, my_loss)
     loss.backward()
     my_grad = my_crit.backward()
-    print(my_grad)
     my_net.backward(my_grad)
-    print(net[0].weight.grad, net[0].bias.grad)
-    print(my_net.bias_grad, my_net.weight_grad)
+    for i in [0, 2]:
+        print("torch ", net[i].weight.grad, net[i].bias.grad)
+        print("my ", my_net.layers[i].weight_grad, my_net.layers[i].bias_grad)
 
